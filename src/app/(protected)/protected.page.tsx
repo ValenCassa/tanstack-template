fb274e8f2b38b6b40019b2d7fbac6c6a4450be7f
Valentin Cassarino <valencassa@gmail.com>
2025-08-27 16:46:57 -0300

    Added JSON stringify

--- src/app/(protected)/protected.page.tsx
+++ src/app/(protected)/protected.page.tsx
@@ -5,9 +5,11 @@ export const Route = createFileRoute("/_(protected).layout/protected")({
 });
 
 function RouteComponent() {
+  const { session } = Route.useRouteContext();
   return (
-    <div className="grid h-full w-full place-content-center text-sm">
+    <div className="max-w-app mx-auto grid h-full w-full place-content-center text-sm">
       This is a protected route!
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
